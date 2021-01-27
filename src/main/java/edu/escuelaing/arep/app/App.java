530ba054231628d2283520ac05a55fd7972544e9
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-01-27 12:44:09 -0500

    calculos media y desviacion

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,13 +1,22 @@
 package edu.escuelaing.arep.app;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static double media( List<Double> listaMedia ){
+    	double cont=0.0;
+        for(double i: listaMedia) {
+        	cont+=i;
+        }
+        return cont/listaMedia.size();
     }
+	public static double desviacion( List<Double> listaDesviacion ){
+		double cont = 0.0;
+		double promedio= media(listaDesviacion);
+		for(double i: listaDesviacion) {
+        	cont+=Math.pow((i*promedio),2);
+        }
+		return Math.sqrt(cont/(listaDesviacion.size()-1));
+	}
 }
